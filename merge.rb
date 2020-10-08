d31b8beb3fd45bfaa768258284baaa0982921f9d
Nicholas <69174708+berubenic@users.noreply.github.com>
2020-10-08 19:45:09 -0400

    created merge_sort def

--- merge.rb
+++ merge.rb
@@ -1,13 +1,3 @@
 # frozen_string_literal: true
 
-def merge_sort(arr)
-  return arr if arr.length < 2
-
-  left = arr[0..(arr.length / 2 - 1)]
-  right = arr[(arr.length / 2)..-1]
-
-  p left
-  p right
-end
-
-merge_sort([2, 1, 6, 7, 10, 3, 5, 8])
+def merge_sort; end
