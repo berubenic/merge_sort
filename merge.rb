346c9a36277a4d776e7a3a9955fb13b57b19f58a
Nicholas <69174708+berubenic@users.noreply.github.com>
2020-10-08 19:55:23 -0400

    array getes split in two halfs, sorts index 0 of left and right

--- merge.rb
+++ merge.rb
@@ -1,3 +1,29 @@
 # frozen_string_literal: true
 
-def merge_sort; end
+def merge_sort(arr)
+  arr if arr.length < 2
+  left = arr[0..arr.length / 2 - 1]
+  right = arr[arr.length / 2..-1]
+  p left
+  p right
+  merge(left, right)
+end
+
+def merge(left, right, arr = [])
+  if left.length == 1
+    return left
+  elsif right.length == 1
+    return right
+  end
+
+  if left[0] < right[0]
+    arr.push(right[0])
+    arr.push(left[0])
+  else
+    arr.push(left[0])
+    arr.push(right[0])
+  end
+  p arr
+end
+
+merge_sort([9, 8, 7, 6, 5, 4, 3, 2])
