441da32532c1064e129617347bbc651f1a0cb058
Nicholas <69174708+berubenic@users.noreply.github.com>
2020-10-10 17:29:44 -0400

    merging two sorted arrays works

--- merge.rb
+++ merge.rb
@@ -1,29 +1,43 @@
 # frozen_string_literal: true
 
 def merge_sort(arr)
-  arr if arr.length < 2
-  left = arr[0..arr.length / 2 - 1]
-  right = arr[arr.length / 2..-1]
-  p left
-  p right
-  merge(left, right)
+  if arr.length > 1
+    left = arr[0..arr.length / 2 - 1]
+    right = arr[arr.length / 2..-1]
+    merge_sort(left)
+    merge_sort(right)
+    merge(left, right)
+  end
 end
 
-def merge(left, right, arr = [])
-  if left.length == 1
-    return left
-  elsif right.length == 1
-    return right
-  end
+def merge(left, right)
+  result = []
+  i = 0
+  j = 0
+  k = 0
 
-  if left[0] < right[0]
-    arr.push(right[0])
-    arr.push(left[0])
-  else
-    arr.push(left[0])
-    arr.push(right[0])
+  while i < left.length && j < right.length
+    if left[i] < right[j]
+      result[k] = left[i]
+      k += 1
+      i += 1
+    else
+      result[k] = right[j]
+      k += 1
+      j += 1
+    end
   end
-  p arr
+  until i == left.length
+    result[k] = left[i]
+    k += 1
+    i += 1
+  end
+  until j == right.length
+    result[k] = right[j]
+    k += 1
+    j += 1
+  end
 end
 
-merge_sort([9, 8, 7, 6, 5, 4, 3, 2])
+p merge_sort([5, 7, 9, 2, 6, 3, 5, 5])
+p merge([2, 4, 7], [1, 2, 4])
